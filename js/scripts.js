3cb2c161dc7af9601326d4c9a8dba16cb6b5db5a
caio1902araujo <caio2002araujo@gmail.com>
2021-10-25 23:53:48 -0300

    adding js logic from the mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,49 @@ function startScrollSmooth(){
     });
   }
 
-  internalLinks.forEach((link) => link.addEventListener('click', scrollSmooth))
+  internalLinks.forEach((link) => link.addEventListener("click", scrollSmooth))
+}
+
+function startClickOut(navigation, dataAttribute, removeActiveClass){
+  const html = document.documentElement;
+
+  function clickOut(event){
+    if(!navigation.contains(event.target)){
+      html.removeEventListener('click', clickOut);
+      html.removeAttribute(dataAttribute);
+      removeActiveClass()
+    }
+  }
+
+  if(!html.hasAttribute(dataAttribute)){
+    setTimeout(() => html.addEventListener('click', clickOut));
+    html.setAttribute(dataAttribute, '');
+  }
+
+}
+
+function startMobileMenu(){
+  const buttonHamburger = document.querySelector("[data-menu='button']");
+  const navigation = document.querySelector("[data-menu='navigation']");
+  const dataAttribute = 'data-out'
+  const removeActiveClass = () => {
+    navigation.classList.remove('active');
+    buttonHamburger.classList.remove('active');
+  }
+
+  function handleClick(){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+    startClickOut(navigation, dataAttribute, removeActiveClass)
+  }
+
+  buttonHamburger.addEventListener('click', handleClick);
 }
 
 
 function init(){
   startScrollSmooth();
+  startMobileMenu();
 }
 
 init();
