b83f18423c864e0f662f35ba810289966bf1fd23
caio1902araujo <caio2002araujo@gmail.com>
2021-10-31 13:47:07 -0300

    changing endpoint to send the question

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ function startSendQuestion(){
     }, 0)
     
     if(isValid === Object.keys(formDate).length){
-      const response = await fetch(`http://127.0.0.1:5000/enviar-duvida`, {
+      const response = await fetch(`https://crosslifeapi.herokuapp.com/enviar-duvida`, {
         method: 'POST',
         headers:{'Content-Type': 'application/json'},
         body: JSON.stringify(formDate),
